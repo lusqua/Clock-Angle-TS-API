f93db67258a69fc2fb11c1ff5302f0d0177640c3
lusqua <lucasaguiar.floripa@gmail.com>
2022-09-25 01:16:24 -0300

    Feat. test of fetchMinutes route

---
+++ src/infra/http/controller/__tests__/fetchMinutes.test.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from "express";
+
+import { IClockCalcsFindService } from "../../../../domain/useCase/ClockCalsFindService";
+import { ClockAngleMinutesFetch } from "../fetchMinutes";
+
+describe('Test fetch hour route', () => {
+
+  let clockCalcsFind: IClockCalcsFindService;
+
+  beforeEach(() => {
+    jest.useFakeTimers()
+
+    clockCalcsFind = {
+      handle: jest.fn().mockReturnValue({
+        angle: 180
+      }),
+    };
+  });
+
+  it('should be 180 degree', async () => {
+
+    const fectch = new ClockAngleMinutesFetch(clockCalcsFind);
+
+    const req = {
+      params: {
+        hour: 6
+      }
+    } as unknown;
+
+    const res = {
+      send: jest.fn()
+    } as unknown;
+
+
+    const response = await fectch.handle(req as Request, res as Response);
+
+    expect(response.angle).toBe(180);
+  });
+});
